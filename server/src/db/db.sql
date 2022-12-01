2703af11bd066e949a7b384b301a6131b485cdcf
Craig Edney <caedney@btinternet.com>
2022-12-01 09:16:59 +0000

    feat: Add new reviews table

--- server/src/db/db.sql
+++ server/src/db/db.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE yelp;
 
+------------------------------------------------
+
 CREATE TABLE restaurants (
 	id BIGSERIAL NOT NULL PRIMARY KEY,
 	name VARCHAR(50) NOT NULL,
@@ -12,3 +14,19 @@ ALTER SEQUENCE restaurants_id_seq RESTART WITH 1;
 INSERT INTO restaurants (name, location, price_range) VALUES ('The Oatlands Chaser', 'Weybridge', 2);
 INSERT INTO restaurants (name, location, price_range) VALUES ('The Running Mare', 'Byfleet', 2);
 INSERT INTO restaurants (name, location, price_range) VALUES ('The Ivy', 'Byfleet', 1);
+
+------------------------------------------------
+
+CREATE TABLE reviews (
+	id BIGSERIAL NOT NULL,
+	restaurant_id BIGINT NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	review TEXT NOT NULL,
+	rating INT CHECK(rating >= 1 AND rating <= 5) NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY(restaurant_id) REFERENCES restaurants(id)
+);
+
+INSERT INTO reviews (restaurant_id, name, review, rating) VALUES (1, 'Jack', 'Restaurant was amazing!', 5);
+
+------------------------------------------------
